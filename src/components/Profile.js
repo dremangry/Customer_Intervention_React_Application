bc3d55e90c65848d0716a7d9aec8fb068e7ef6d0
dremangry <andrezreyes2015@gmail.com>
2022-05-01 12:52:47 -0400

    test

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,22 +8,59 @@ const Profile = () => {
     return (
         <div className="container">
             <header className="jumbotron">
-                <h3>
+                {/* <h3>
                     <strong>{currentUser.email}</strong> Profile
+                </h3> */}
+                <h3>
+                    Your Profile
                 </h3>
             </header>
 
             <p>
-                <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
-                {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
+                <strong>Token:</strong> {currentUser.access_token}
+                {/* substring(0, 20)} ...{" "}
+                {currentUser.access_token.substr(currentUser.access_token.length - 20 */}
             </p>
             <p>
                 <strong>Id:</strong> {currentUser.id}
             </p>
             <p>
-                <strong>Email:</strong> {currentUser.email}
+                <strong>buildingId:</strong> {currentUser.buildingId}
             </p>
-            <strong>Authorities:</strong>
+            <p>
+                <strong>batteryId:</strong> {currentUser.batteryId}
+            </p>
+            <p>
+                <strong>columnId:</strong> {currentUser.columnId}
+            </p>
+            <p>
+                <strong>elevatorId:</strong> {currentUser.elevatorId}
+            </p>
+            <p>
+                <strong>results:</strong> {currentUser.results}
+            </p>
+            <p>
+                <strong>report:</strong> {currentUser.report}
+            </p>
+            <p>
+                <strong>status:</strong> {currentUser.status}
+            </p>
+            <p>
+                <strong>authorId:</strong> {currentUser.authorId}
+            </p>
+            <p>
+                <strong>employeeId:</strong> {currentUser.employeeId}
+            </p>
+            <p>
+                <strong>customerId:</strong> {currentUser.customerId}
+            </p>
+            <p>
+                <strong>createdAt:</strong> {currentUser.createdAt}
+            </p>
+            <p>
+                <strong>updatedAt:</strong> {currentUser.updatedAt}
+            </p>
+            {/* <strong>Authorities:</strong> */}
             <ul>
                 {currentUser.roles &&
                     currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -8,17 +8,31 @@ getCurrentUser(): get stored user information (including JWT)
 */
 
 import axios from "axios";
+import querystring from "query-string"
 // const API_URL = "http://localhost:8080/api/auth/";
 const API_AUTH_URL = "https://thisisrocketelevatorsjava.herokuapp.com/authenticate";
+// const API_USER_URL = "https://thisisrocketelevatorsjava.herokuapp.com/interventions/customer?customerId=1";
+const API_USER_URL = "https://rocketelevatorsweek8.azurewebsites.net/api/intervention/pending";
 // const API_AUTH_URL = "http://localhost:3500/authentication/2/";
 
 const login = (email, password) => {
+
+    axios.get("https://thisisrocketelevatorsjava.herokuapp.com/users/hello").then(response => console.log(response.data));
+    console.log(email)
+    console.log(password)
+
     return axios
-        .post(API_AUTH_URL, {
+        .post(API_AUTH_URL, querystring.stringify({
             email: email,
             password: password
-        })
+        }),
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            })
         .then((response) => {
+            console.log(response.data);
             console.log('$$$$$$$$$$$$$$yup');
             console.log('yes#########');
             if (response.data.access_token) {
@@ -30,9 +44,14 @@ const login = (email, password) => {
 const logout = () => {
     localStorage.removeItem("user");
 };
-const getCurrentUser = () => {
-    return JSON.parse(localStorage.getItem("user"));
+// const getCurrentUser = () => {
+//     return JSON.parse(localStorage.getItem("user"));
+// };
+const getCurrentUser  = () =>  {
+    
+    return  axios.get(API_USER_URL).then(response => console.log(response.data));
 };
+
 const AuthService = {
     // register,
     login,
